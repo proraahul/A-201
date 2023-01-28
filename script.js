09c1080c60daeb482dfa522f47a085a207303bec
proraahul <108223821+proraahul@users.noreply.github.com>
2023-01-28 15:38:27 +0000

    corrected

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ let arr = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
   { id: 3, name: "karen", age: "19", profession: "admin" },
-];
+];  
 
 function PrintDeveloperswithMap() {
+//console.log all the employees who have the profession of a developer using the map function.
+  let developers = arr.map((employee)=>{
+    if(employee.profession === "developer"){
+      return item;
+    }
+  });
+  console.log(developers);
+}
+
+function PrintDeveloperswithFilter() {
   //Write your code here
+} 
 }
 
 function PrintDeveloperbyForEach() {
